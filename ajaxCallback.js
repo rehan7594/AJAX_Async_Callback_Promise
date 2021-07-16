c89c6a42a1c75d10e2b600dfc5329150394636a0
rehan7594 <rehansdm94@gmail.com>
2021-07-15 20:45:55 -0700

    ADDED: Demonstrate Async nature of JavaScript using AJAX, PROMISE and REST services using JSON server

---
+++ ajaxCallback.js
@@ -0,0 +1,56 @@
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+function showTime() {
+    const date = new Date();
+    return date.getHours() + "Hrs:" + date.getMinutes() + "Mins:" + date.getSeconds() + "Secs";
+}
+
+function makeAJAXCall(methodType, url, callback, async = true, data = null) {
+    const xhr = new XMLHttpRequest();
+    
+       xhr.onreadystatechange = function() {
+        
+        //When status code is 200,201 then only do a callback --> getUserDetails()
+        if (xhr.readyState === 4){
+        if (xhr.status === 200 || xhr.status === 201) {
+            callback(xhr.responseText);
+        } else if (xhr.status >= 400) {
+            console.log("Handle 400 Client Error or 500 Server Error at: " +showTime());
+        }
+    }
+    }
+
+    xhr.open(methodType, url, async);
+    if (data) {
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+    } else xhr.send();
+    console.log(methodType+ " request sent to the server at: " +showTime());
+}
+
+
+const getURL =  "http://127.0.0.1:3000/employees/1";
+function getUserDetails(data) {
+    console.log("Get User Data at: " + showTime() + " data: " +data);
+}
+
+makeAJAXCall("GET", getURL, getUserDetails, true);
+//executes before the state change 
+console.log("Made GET AJAX call to Server at " +showTime());
+
+const deleteURL = "http://127.0.0.1:3000/employees/4";
+function userDeleted(data) {
+    console.log("User Deleted at: " + showTime() + " data: " +data);
+}
+// To execut post call only after delete call-->(false)
+makeAJAXCall("DELETE", deleteURL, userDeleted, false);
+console.log("Made DELETE AJAX call to Server at " +showTime());
+
+// Id is created by the server
+const postURL = "http://127.0.0.1:3000/employees";
+const emplData = {"name": "Harry", "salary": "5000"};
+function userAdded(data) {
+    console.log("User Added at: " + showTime() + " data: " +data);
+}
+makeAJAXCall("POST", postURL, userAdded, true, emplData);
+console.log("Made POST AJAX call to Server at " +showTime());
